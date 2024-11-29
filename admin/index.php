342704d2e2c478810ebe064a5437cb8e1cdfffc2
nguyenminhquan <idolquan04@gmail.com>
2024-11-29 21:33:17 +0700

    code phần php trang danh mục ngày 29/11

--- admin/index.php
+++ admin/index.php
@@ -1,12 +1,39 @@
 <?php
 
+    include "../model/pdo.php";
     include "header.php";
     //controler
 
     if (isset($_GET['act'])){
         $act = $_GET['act'];
         switch ($act){
+
             case 'danhmuc':
+                $sql = "select * from danhmuc order by name";
+                $listdanhmuc = pdo_query($sql);
+                include 'danhmuc/danhmuc.php';
+                break;
+
+            case 'adddm':
+                //check click or 0
+                if (isset($_POST['themmoi'])){
+                    $name = $_POST['name'];
+                    $ghichu = $_POST['ghichu'];
+
+                    if (empty($name) || empty($ghichu)) {
+                        $thongbao= "Vui lòng nhập tên danh mục!";
+                    } else {
+                        $sql = "INSERT INTO danhmuc (name, ghichu) VALUES ('$name', '$ghichu')";
+                        pdo_execute($sql);
+                        $thongbao= "Thêm danh mục thành công!";
+
+                        // Sau khi thêm thành công, chuyển hướng về trang danh mục
+                        header("Location: index.php?act=danhmuc");
+                        exit; // Lưu ý phải có exit sau header để dừng chương trình
+                    }
+                }
+                $sql = "select * from danhmuc order by name";
+                $listdanhmuc = pdo_query($sql);
                 include 'danhmuc/danhmuc.php';
                 break;
             
